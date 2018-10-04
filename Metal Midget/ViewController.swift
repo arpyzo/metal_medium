c5d3ce5c8095de31a4a0fe3e767b94334d93cbfb
Robert Pyzalski <robert.pyzalski@shiftboard.com>
2018-10-03 22:31:43 -0700

    converting to mtkview

--- Metal Midget/ViewController.swift
+++ Metal Midget/ViewController.swift
@@ -12,23 +12,32 @@ import MetalKit
 
 class ViewController: UIViewController {
     var metalDevice: MTLDevice!
-    var metalLayer: CAMetalLayer!
+    //var metalLayer: CAMetalLayer!
     var pipelineState: MTLRenderPipelineState!
     var commandQueue: MTLCommandQueue!
     var textureLoader: MTKTextureLoader!
-    var timer: CADisplayLink!
+    //var timer: CADisplayLink!
     
     var texture: MTLTexture!
     var objectToDraw: Rectangle!
+    
+    @IBOutlet weak var mtkView: MTKView! {
+        didSet {
+            mtkView.delegate = self
+            mtkView.preferredFramesPerSecond = 60
+            mtkView.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         metalDevice = MTLCreateSystemDefaultDevice()
-        metalLayer = CAMetalLayer()
-        metalLayer.device = metalDevice
-        //metalLayer.frame = view.layer.frame
-        view.layer.addSublayer(metalLayer)
+        mtkView.device = metalDevice
+        //metalLayer = CAMetalLayer()
+        //metalLayer.device = metalDevice
+        //metalLayer.frame = view.layer.frame // OLD CODE
+        //view.layer.addSublayer(metalLayer)
         
         textureLoader = MTKTextureLoader(device: metalDevice)
         
@@ -55,11 +64,11 @@ class ViewController: UIViewController {
         
         commandQueue = metalDevice.makeCommandQueue()
         
-        timer = CADisplayLink(target: self, selector: #selector(ViewController.gameLoop))
-        timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+        //timer = CADisplayLink(target: self, selector: #selector(ViewController.gameLoop))
+        //timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
     }
     
-    override func viewDidLayoutSubviews() {
+    /*override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         if let window = view.window {
@@ -73,24 +82,56 @@ class ViewController: UIViewController {
             //projectionMatrix = Matrix4.makePerspectiveViewAngle(Matrix4.degrees(toRad: 85.0), aspectRatio: Float(self.view.bounds.size.width / self.view.bounds.size.height), nearZ: 0.01, farZ: 100.0)
 
         }
-    }
+    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func render() {
+    /*func render() {
         guard let drawable = metalLayer?.nextDrawable() else { return }
         objectToDraw.render(commandQueue: commandQueue, pipelineState: pipelineState, drawable: drawable, texture: texture)
         //objectToDraw.render(commandQueue: commandQueue, pipelineState: pipelineState, drawable: drawable)
+    }*/
+    
+    func render(_ drawable: CAMetalDrawable?) {
+        guard let drawable = drawable else { return }
+        //self.metalViewControllerDelegate?.renderObjects(drawable)
+        objectToDraw.render(commandQueue: commandQueue, pipelineState: pipelineState, drawable: drawable, texture: texture)
     }
 
-    @objc func gameLoop() {
+
+    /*@objc func gameLoop() {
         autoreleasepool {
             self.render()
         }
-    }
+    }*/
 
 }
-
+// MARK: - MTKViewDelegate
+extension ViewController: MTKViewDelegate {
+    
+    // 1
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        if let window = view.window {
+            let scale = window.screen.nativeScale
+            //let layerSize = view.bounds.size
+            view.contentScaleFactor = scale
+            //view.drawableSize = CGSize(width: layerSize.width * scale, height: layerSize.height * scale)
+        }
+        //currentDrawable.drawableSize = CGSize(width: 1.0, height: 1.0)
+        //CGSize(width: layerSize.width * scale, height: layerSize.height * scale)
+    }
+ /*    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        projectionMatrix = float4x4.makePerspectiveViewAngle(float4x4.degrees(toRad: 85.0),
+                                                             aspectRatio: Float(self.view.bounds.size.width / self.view.bounds.size.height),
+                                                             nearZ: 0.01, farZ: 100.0)
+    }*/
+    
+    // 2
+    func draw(in view: MTKView) {
+        render(view.currentDrawable)
+    }
+    
+}
